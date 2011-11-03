2872560d62e1677cf630014dafe5c5b24d106c06
Michael Wawra <michael@wawra.co.uk>
2011-11-03 23:17:47 +0000

    Added basic 5 screen layout.

---
+++ TaxiTaxiApp/TTCheckinViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTSecondViewController.h
+//  TaxiTaxiApp
+//
+//  Created by Michael Wawra on 03/11/2011.
+//  Copyright (c) 2011 Wawra Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTCheckinViewController : UIViewController
+
+@end
---
+++ TaxiTaxiApp/TTMedalsViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTMedalsViewController.h
+//  TaxiTaxiApp
+//
+//  Created by Michael Wawra on 03/11/2011.
+//  Copyright (c) 2011 Wawra Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTMedalsViewController : UIViewController
+
+@end
